39e09c329f749c00c9f67d85431bf5780cf0595f
Parth Sarkar <ps679@cornell.edu>
2024-07-26 16:18:10 -0400

    use int splitting to split seq. blocks arbitrarily many times

--- calyx-opt/src/passes/new_fsm.rs
+++ calyx-opt/src/passes/new_fsm.rs
@@ -46,7 +46,7 @@ impl NewFSMs {
                 let (sub_query, t_next_usize) =
                     ((i - 1, l - 1), (i - 1).try_into().unwrap());
                 let (memoized, subprob_opt) = match map.get(&sub_query) {
-                    // if memoized, use cached val. instead of recomputing
+                    // if possible, use cached val. instead of recomputing
                     Some(v) => (true, *v),
                     None => match (l == 1, i == l) {
                         (true, _) => (
@@ -125,14 +125,28 @@ impl NewFSMs {
     /// 2. All formed groups are contiguous w.r.t. original ordering in `lst`
     /// 3. The sum of the elements in each group are as close as possible to the
     ///    sum of all `n` elements in `lst` divided by the number of groups `k`
-    fn compute_split_indices(lst: &Vec<u64>, num_groups: u64) -> Vec<u64> {
+    ///
+    /// Returns a list of inclusive ranges, whose bounds represent the groups
+    fn compute_split_indices(
+        lst: &Vec<u64>,
+        num_groups: u64,
+    ) -> Vec<(u64, u64)> {
         let (mut map, mut splits) = (HashMap::new(), VecDeque::new());
-        let (sum, len): (u64, u64) =
-            (lst.iter().sum(), lst.len().try_into().unwrap());
-        let (query, avg) = ((len - 1, num_groups - 1), sum / num_groups);
-        Self::compute_opts(lst, &mut map, avg, query);
-        Self::backtrack(&map, lst, &mut splits, avg, query);
-        Vec::<u64>::from(splits)
+        let (avg, len) = (
+            lst.iter().sum::<u64>() / num_groups,
+            TryInto::<u64>::try_into(lst.len()).unwrap(),
+        );
+        Self::compute_opts(lst, &mut map, avg, (len - 1, num_groups - 1));
+        Self::backtrack(&map, lst, &mut splits, avg, (len - 1, num_groups - 1));
+        let mut prev_index = 0;
+        splits
+            .iter()
+            .map(|i| {
+                let range = (prev_index, *i);
+                prev_index = *i + 1;
+                range
+            })
+            .collect()
     }
 }
 
@@ -191,106 +205,40 @@ impl Visitor for NewFSMs {
         _sigs: &LibrarySignatures,
         _comps: &[ir::Component],
     ) -> VisResult {
-        // Decide where to split based on the total number of Invoke Control objects we find.
-        // Store approx sizes of each statement so we know where to split
+        // Stores an approx size of each statement in the Seq block
+        let stmt_sizes: Vec<u64> = s
+            .stmts
+            .iter()
+            .map(|stmt| {
+                stmt.approx_size(
+                    APPROX_ENABLE_SIZE,
+                    APPROX_WHILE_REPEAT_SIZE,
+                    APPROX_IF_SIZE,
+                )
+            })
+            .collect();
 
-        // stores an approx size of each statement in the Seq block
-        let mut statement_sizes: Vec<u64> = Vec::new();
-
-        // element i stores size_1 + ... + size_i
-        let mut total_sizes: Vec<u64> = Vec::new();
-
-        // stores total approx size of the Seq block
-        let mut total_size: u64 = 0;
-
-        for stmt in s.stmts.iter() {
-            let approx_stmt_size: u64 = ir::Control::approx_size(
-                stmt,
-                APPROX_ENABLE_SIZE,
-                APPROX_WHILE_REPEAT_SIZE,
-                APPROX_IF_SIZE,
-            );
-
-            statement_sizes.push(approx_stmt_size);
-            total_size += approx_stmt_size;
-            total_sizes.push(total_size);
-        }
-
-        // exit out if threshold for splitting exceeds the estimated total size
-        if total_size < self.threshold {
+        // Exit out if threshold for splitting exceeds the estimated total size
+        if stmt_sizes.iter().sum::<u64>() < self.threshold {
             return Ok(Action::Continue);
         }
 
-        // logic to find the index of the seq which yields the best split
-        let (min_indx_opt, _, _) = total_sizes
-            .iter()
-            .map(|sub_total| {
-                (*sub_total).abs_diff(total_size.abs_diff(*sub_total))
-            })
-            .fold(
-                (None, None, 0),
-                |(min_indx_opt, min_diff_opt, curr_indx), curr_diff| match (
-                    min_indx_opt,
-                    min_diff_opt,
-                ) {
-                    (None, None) => {
-                        (Some(curr_indx), Some(curr_diff), curr_indx + 1)
-                    }
-                    (Some(min_indx), Some(min_diff)) => {
-                        if curr_diff < min_diff {
-                            (Some(curr_indx), Some(curr_diff), curr_indx + 1)
-                        } else {
-                            (Some(min_indx), Some(min_diff), curr_indx + 1)
-                        }
-                    }
-                    _ => unreachable!(),
-                },
-            );
-
-        // at best split, split seq into first and second sub-seq blocks, each of which get @new_fsm
-        match min_indx_opt {
-            None => Ok(Action::Continue), // unreachable
-            Some(min_indx) => {
-                let (fst_stmts, snd_stmts) = {
-                    let mut fst: Vec<ir::Control> = Vec::new();
-                    let mut snd: Vec<ir::Control> = Vec::new();
-
-                    for (i, c) in s.stmts.drain(..).enumerate() {
-                        if i <= (min_indx as usize) {
-                            fst.push(c)
-                        } else {
-                            snd.push(c)
-                        }
-                    }
-
-                    (fst, snd)
-                };
-
-                // place @new_fsm attribute at children seq
-                let mut child_attrs = s.attributes.clone();
-                child_attrs.insert(ir::BoolAttr::NewFSM, 1);
-
-                // give first child seq @new_fsm attribute as well
-                let fst_seq = ir::Control::Seq(ir::Seq {
-                    stmts: fst_stmts,
-                    attributes: child_attrs.clone(),
-                });
-
-                // same for second child seq
-                let snd_seq = ir::Control::Seq(ir::Seq {
-                    stmts: snd_stmts,
-                    attributes: child_attrs.clone(),
-                });
-
-                // change parent statements to have exactly two children seqs according to split, along with old attributes
-                let parent_seq = ir::Control::Seq(ir::Seq {
-                    stmts: vec![fst_seq, snd_seq],
-                    attributes: s.attributes.clone(),
-                });
-                let split_seqs = Action::change(parent_seq);
-
-                Ok(split_seqs)
-            }
-        }
+        // Split the `seq` block into children `seq`s controlled by a parent
+        let parent_seq = ir::Control::Seq(ir::Seq {
+            stmts: Self::compute_split_indices(&stmt_sizes, self.num_children)
+                .drain(..)
+                .map(|(lb, ub)| {
+                    let r = ((ub - lb) + 1).try_into().unwrap();
+                    let mut child_attrs = s.attributes.clone();
+                    child_attrs.insert(ir::BoolAttr::NewFSM, 1);
+                    ir::Control::Seq(ir::Seq {
+                        stmts: s.stmts.drain(0..r).collect(),
+                        attributes: child_attrs,
+                    })
+                })
+                .collect(),
+            attributes: s.attributes.clone(),
+        });
+        Ok(Action::change(parent_seq))
     }
 }
